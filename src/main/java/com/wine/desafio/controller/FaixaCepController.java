0f4c52e25533404efc0690048ac6b2dc49cf5c88
Ubiratan Roberte Cardoso Passos <ubiratan.passos@globalsys.com.br>
2021-04-21 21:42:17 -0300

    arquivos faltando

--- src/main/java/com/wine/desafio/controller/FaixaCepController.java
+++ src/main/java/com/wine/desafio/controller/FaixaCepController.java
@@ -3,6 +3,7 @@ package com.wine.desafio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,27 +22,32 @@ public class FaixaCepController {
 
 	@Autowired
 	private FaixaCepService faixaCepService;
-		
+	
+	@CrossOrigin
 	@PostMapping("/loja")
 	public FaixaCepDTO salvaLoja(@RequestBody FaixaCepDTO loja){
 		return faixaCepService.salvar(loja);
 	} 
 	
+	@CrossOrigin
 	@GetMapping("/listalojas")
 	public List<FaixaCepDTO> listaLojasAll(){
 		return faixaCepService.listarLojas();
 	} 
 	
+	@CrossOrigin
 	@GetMapping("/recuperaLoja/{cep}")
 	public FaixaCepDTO lojaPorCep(@PathVariable("cep") Integer cep){
 		return faixaCepService.recuperaLojaporCep(cep);
 	} 
 	
+	@CrossOrigin
 	@PatchMapping("/loja/{idLoja}")
 	public FaixaCepDTO lojaPorCep(@RequestBody FaixaCepDTO loja, @PathVariable("idLoja") Long idLoja){
 		return faixaCepService.editar(idLoja, loja);
 	} 	
 	
+	@CrossOrigin
 	@DeleteMapping("/loja/{idLoja}")
 	public void removeLoja(@PathVariable("idLoja") Long idLoja){
 		faixaCepService.excluir(idLoja);
